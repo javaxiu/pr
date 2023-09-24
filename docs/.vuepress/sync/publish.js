e0040099fe9f4d89ab25712fa6da580be80fca23
jiawenliang <jiawenliang@tencent.com>
2023-09-24 18:18:47 +0800

    init

--- docs/.vuepress/sync/publish.js
+++ docs/.vuepress/sync/publish.js
@@ -1,12 +1,5 @@
 const ghPages = require('gh-pages');
-const childProcess = require('child_process');
 
-childProcess.exec('npm run build', (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  ghPages.publish('./docs/.vuepress/dist', { branch: 'gh-page' }, (err) => {
-    console.log(err);
-  })
-});
+ghPages.publish('./docs/.vuepress/dist', { branch: 'gh-page' }, (err) => {
+  console.log(err);
+})
